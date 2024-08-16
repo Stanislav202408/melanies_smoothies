22e0296e0c5f7aeaa9a45eef80453f85fceff8de
Stanislav202408 <stanislav.opatrny@photonenergy.com>
2024-08-16 14:44:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,12 +60,12 @@ if ingredients_list:
 
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
-        st.write(my_insert_stmt)
+
         dwh = "USE WAREHOUSE COMPUTE_DWH;"
         st.write(dwh)
-        #session.sql("USE WAREHOUSE COMPUTE_DWH").collect()
         session.sql(dwh).collect()
         session.sql(my_insert_stmt).collect()
+        st.write(my_insert_stmt)  
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
