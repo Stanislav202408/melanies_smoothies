2b06d8c563c2247d843160167b270dd79077a824
Stanislav202408 <stanislav.opatrny@photonenergy.com>
2024-08-16 14:21:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,11 +54,12 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
-    my_insert_stmt = """use warehouse COMPUTE_WH; insert into smoothies.public.orders(ingredients,name_on_order)
+    my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
+        session.sql("USE WAREHOUSE COMPUTE_DWH").collect()
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
